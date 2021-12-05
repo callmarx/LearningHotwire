400f4ebe81035f66a928f0977731b5e79401c47f
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2021-12-05 18:25:30 -0300

    chore(seed): differents created_at

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,26 +9,32 @@
 Chore.create([
   {
     title: "Gave up",
-    content: "Would it save you a lot of time if I just gave up and went mad now?"
+    content: "Would it save you a lot of time if I just gave up and went mad now?",
+    created_at: 15.days.ago
   },
   {
     title: "More than big",
-    content: "Space is big. You just won't believe how vastly, hugely, mind-bogglingly big it is. I mean, you may think it's a long way down the road to the chemist's, but that's just peanuts to space."
+    content: "Space is big. You just won't believe how vastly, hugely, mind-bogglingly big it is. I mean, you may think it's a long way down the road to the chemist's, but that's just peanuts to space.",
+    created_at: 4.days.ago
   },
   {
     title: "What did happen?",
-    content: "For a moment, nothing happened. Then, after a second or so, nothing continued to happen."
+    content: "For a moment, nothing happened. Then, after a second or so, nothing continued to happen.",
+    created_at: 9.days.ago
   },
   {
     title: "About be President",
-    content: "Anyone who is capable of getting themselves made President should on no account be allowed to do the job."
+    content: "Anyone who is capable of getting themselves made President should on no account be allowed to do the job.",
+    created_at: 36.days.ago
   },
   {
     title: "UX/UI",
-    content: "A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools"
+    content: "A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools",
+    created_at: 6.days.ago
   },
   {
     title: "42",
-    content: "Forty-two."
+    content: "Forty-two.",
+    created_at: 1.days.ago
   },
 ])
