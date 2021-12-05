15475a783f615319d32b1983804afb2ca1e48caf
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2021-12-05 17:50:09 -0300

    fix(lint): apply rubocop lint
     - fixed with command:
       rubocop -A

--- spec/views/chores/edit.html.erb_spec.rb
+++ spec/views/chores/edit.html.erb_spec.rb
@@ -1,4 +1,6 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe "chores/edit", type: :view do
   before(:each) do
@@ -12,7 +14,6 @@ RSpec.describe "chores/edit", type: :view do
     render
 
     assert_select "form[action=?][method=?]", chore_path(@chore), "post" do
-
       assert_select "input[name=?]", "chore[title]"
 
       assert_select "textarea[name=?]", "chore[content]"
--- spec/views/chores/new.html.erb_spec.rb
+++ spec/views/chores/new.html.erb_spec.rb
@@ -1,4 +1,6 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe "chores/new", type: :view do
   before(:each) do
@@ -12,7 +14,6 @@ RSpec.describe "chores/new", type: :view do
     render
 
     assert_select "form[action=?][method=?]", chores_path, "post" do
-
       assert_select "input[name=?]", "chore[title]"
 
       assert_select "textarea[name=?]", "chore[content]"
