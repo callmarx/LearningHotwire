27c145c2e6d7bc8ce6ef3f9316127ea45b6d96cc
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2022-06-28 22:27:20 -0300

    feat(stimulus): handle with turbo event
     - Close model after a successful chore insert

--- app/javascript/controllers/chore_modal_controller.js
+++ app/javascript/controllers/chore_modal_controller.js
@@ -2,14 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="chore-modal"
 export default class extends Controller {
-  connect() {
-    console.log("Hi! we are in ChoreModalController from Stimulus")
-  }
-
   // action: "chore-modal#hideModal"
   hideModal() {
     this.element.parentElement.removeAttribute("src")
     this.element.remove()
     console.log("You've just called ChoreModalController#hideModal")
+  }
+
+  // action: "turbo:submit-end->chore-modal#submitEnd"
+  submitEnd(e) {
+    if (e.detail.success) {
+      this.hideModal()
+    }
   }
 }
